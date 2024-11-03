b169122acdf6a20311d9810c91200a4ec35b54d8
rchand7 <150305815+rchand7@users.noreply.github.com>
2024-11-03 21:53:32 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: 'http://localhost:5173', // change this to the frontend URL in production
+    origin: 'https://naukriyapao7-1.onrender.com', // change this to the frontend URL in production
     credentials: true
 };
 app.use(cors(corsOptions));
